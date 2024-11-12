9b7289282ec7bb823d9e209805304fc074a4b9c9
Patryk2899 <pjozwiak2899@gmail.com>
2024-11-12 21:17:35 +0100

    Initial commit

---
+++ src/main/java/com/example/cardSearch/CardSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardSearchApplication.class, args);
+	}
+
+}
